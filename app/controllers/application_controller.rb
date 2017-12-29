dacb0c7b8f92f4e596b0392b7f8083a849c98a01
dv6 <belora@nxt.ru>
2017-12-29 15:32:53 +0300

    tst

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,12 @@ class ApplicationController < ActionController::Base
 
   before_action :gon_user, unless: :devise_controller?
 
+
   private
 
   def gon_user
     gon.user_id = current_user.id if current_user
+    gon.access = can?(action_name.to_sym, controller_name.classify.constantize)
   end
 
   rescue_from CanCan::AccessDenied do |e|
